38ef4085978f65e2ff71a13cedf0f6e07acf88d3
peterm <peterm@iovox.com>
2025-09-05 16:20:03 +0100

    Ensured that resetting the organisation filter doesn't attempt to filter by the UI text for the default option

--- resources/views/customers/index.blade.php
+++ resources/views/customers/index.blade.php
@@ -41,7 +41,7 @@
                         class="mt-4 md:mt-0 md:ml-4 py-0 h-10 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full px-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                         onchange="this.form.submit()"
                     >
-                        <option>Filter by organisation...</option>
+                        <option value="">Filter by organisation...</option>
                         @foreach ($organisations as $organisation)
                             <option value="{{ $organisation }}" {{ request('filter.organisation') == $organisation ? 'selected' : '' }}>{{ $organisation }}</option>
                         @endforeach
